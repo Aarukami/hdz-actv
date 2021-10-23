fdd3f992040aa4c644c3214899db61e642dc6e98
Aarukami <91074890+Aarukami@users.noreply.github.com>
2021-10-23 14:54:42 +0530

    Update sample_config.py

--- YoneRobot/sample_config.py
+++ YoneRobot/sample_config.py
@@ -19,7 +19,7 @@ class Config(object):
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 2021224869  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "girls_lob"
-    SUPPORT_CHAT = "Kora_Support"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "Komi_Support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001513602613
     )  # Prints any new group the bot is added to, prints just the name and ID.
